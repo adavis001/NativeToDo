3ac754f897fc7fe6658aa228489335e569dc9b4c
Alex Davis <akonidavis@gmail.com>
2017-08-30 16:08:10 -1000

    todo list finished?

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ListView from './src/ListView';
 
-export default class App extends React.Component {
+class TodoApp extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ListView></ListView>
       </View>
     );
   }
@@ -16,8 +15,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingLeft: 2,
+    paddingRight: 2,
+    backgroundColor: '#F8F8F8',
+  }
 });
